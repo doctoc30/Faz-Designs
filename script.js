0446ea16c40e2e781ccafda76952fff4f9e2baf5
doctoc30 <fazhoq1977@gmail.com>
2025-05-20 09:46:02 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const elements = document.querySelectorAll(".wow");
 
+  const observer = new IntersectionObserver((entries) => {
+    entries.forEach(entry => {
+      if(entry.isIntersecting) {
+        entry.target.classList.add("animate");
+      }
+    });
+  }, { threshold: 0.1 });
+
+  elements.forEach(el => observer.observe(el));
+});
