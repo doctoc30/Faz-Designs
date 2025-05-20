76972e6c76c7165a059c6acf7366f69e977c890d
doctoc30 <fazhoq1977@gmail.com>
2025-05-20 18:11:36 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,4 @@
-document.addEventListener("DOMContentLoaded", () => {
-  const elements = document.querySelectorAll(".wow");
-
-  const observer = new IntersectionObserver((entries) => {
-    entries.forEach(entry => {
-      if(entry.isIntersecting) {
-        entry.target.classList.add("animate");
-      }
-    });
-  }, { threshold: 0.1 });
-
-  elements.forEach(el => observer.observe(el));
-});
+function toggleMenu() {
+  const nav = document.getElementById('nav');
+  nav.classList.toggle('show');
+}
